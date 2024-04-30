0b52187d0765e38da2aeded6b15c868c9b858f8c
Igor Olkhovskiy <i.v.olkhovsky@gmail.com>
2024-04-30 21:34:15 +0300

    Same, minor

--- docker/install.sh
+++ docker/install.sh
@@ -4,4 +4,5 @@ apt update && apt upgrade
 apt install -y build-essential gdb cmake
 apt install -y python3 python3-pip python3-venv
 apt install -y libgl1-mesa-glx
+apt install -y libboost-all-dev libpython3-dev
 pip3 install -r requirements.txt
